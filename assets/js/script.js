7c32b153431b05c885ce3a8cb766eb86888980e8
abelzer09 <aaronrbelzer@gmail.com>
2022-04-02 17:41:43 -0500

    added ids for main tag on html. created funtion to fetch data from api, created some variable for ids passed from html.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,42 @@
 var APIKey = "65593a96c96d3428f2a08b76a4e29e4d";
-var city;
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+var cityEl = document.getElementById("city");
+var currentEl = document.getElementById("Current")
+var dateEl = document.getElementById("date")
+var iconEl = document.getElementById("icon")
+var data1El = document.getElementById('data1')
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + "minneapolis" + "&appid=" + APIKey
+
+
 
 fetch(queryURL)
+.then(function (response) {
+	if (response.ok) {
+		return response.json();
+	} else {
+		return Promise.reject(response);
+	}
+})
+.then(function (data) {
+      var lat = data.coord.lat
+      var lon = data.coord.lon
+	return fetch("https://api.openweathermap.org/data/2.5/onecall?" + "lat=" + lat + "&lon=" + lon +"&exclude=minutely,hourly,alerts" +"&appid=" + APIKey + "&units=imperial");
+})
+.then(function (response) {
+	if (response.ok) {
+		return response.json();
+	} else {
+		return Promise.reject(response);
+	}
+}).then(function (data1) {
+    console.log(data1);
+    for (let i = 0; i < data1.length; i++) {
+        var date = data1[i].current.dt
+        console.log(date)
+        var reformatDate = moment(date, "X" ).format("l")
+        dateEl.textContent = reformatDate
+        // dateEl.append(reformatDate)
+        
+    }
+}).catch(function (error) {
+	console.warn(error);
+});
---
+++ pseudocodehw6.md
@@ -0,0 +1,31 @@
+set up event handler for search button  
+    listen for clicks when button is clicked
+    put searched city into funtion for api
+    save city to local storage and render as button 
+        when city button is clicked display current and future conditions
+            city is not added again to saved city searches
+
+make function to pull required data from api
+    when city is seached display in main
+    display todays date with city name
+        weather icon for current weather
+        temperature
+        wind
+        humidity
+        uv index
+            color code uv index for favorable moderate or severe
+
+when city is searched display 5 day forcast in cards
+    dates
+    weather icon
+    temperature
+    wind
+    humidity
+
+
+
+
+
+http://api.openweathermap.org/data/2.5/weather?q=
+
+    api.openweathermap.org/data/2.5/onecall?lat=30.489772&lon=-99.771335&units=imperial
