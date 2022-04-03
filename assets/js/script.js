bd7afaf8b661164e33471a5cbf2db2c624f5a208
abelzer09 <aaronrbelzer@gmail.com>
2022-04-03 12:41:02 -0500

    added more variables to html and linked them in script.js, completed fuction to pull necessay data from api. api data is displayed on html page. searched cities are saved to local storage and diplayed as buttons on page.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,20 @@
 var APIKey = "65593a96c96d3428f2a08b76a4e29e4d";
 var cityEl = document.getElementById("city");
-var currentEl = document.getElementById("Current")
-var dateEl = document.getElementById("date")
-var iconEl = document.getElementById("icon")
-var data1El = document.getElementById('data1')
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + "minneapolis" + "&appid=" + APIKey
+var currentEl = document.getElementById("Current");
+var dateEl = document.getElementById("currentDate");
+var currentIconEl = document.getElementById("currentIcon")
+var currentTemp1El = document.getElementById('currentTemp')
+var currentWindEl = document.getElementById('currentWind')
+var currentHumEl = document.getElementById('currentHum')
+var currentUvEl = document.getElementById('currentUv')
 
+var BtnSubmit = document.getElementById("submitBtn");
+var citySearch = document.getElementById("search-input")
+var searchCities = JSON.parse(window.localStorage.getItem("cities")) || [];
+console.log(searchCities)
 
-
+function apiCity(city){
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city  + "&appid=" + APIKey
 fetch(queryURL)
 .then(function (response) {
 	if (response.ok) {
@@ -28,15 +35,64 @@ fetch(queryURL)
 		return Promise.reject(response);
 	}
 }).then(function (data1) {
+    var date = data1.daily[0].dt
+    var reformatDate = moment(date, "X" ).format("l")
+    dateEl.textContent = reformatDate
+    // var cIcon = data1.daily[0].weather[0].icon
+    // currentIconEl.textContent = fetch("http://openweathermap.org/img/wn/"+ cIcon +"@2x.png")
+    var cTemp = data1.daily[0].temp.max;
+    currentTemp1El.textContent = "Temp: " + cTemp + " °F"
+    var cWind = data1.daily[0].wind_speed
+    currentWindEl.textContent = "Wind: " + cWind + " MPH"
+    var cHum = data1.daily[0].humidity
+    currentHumEl.textContent = "Humidity: " + cHum + " %"
+    var cUv = data1.daily[0].uvi
+    currentUvEl.textContent = "UV Index: " + cUv
     console.log(data1);
-    for (let i = 0; i < data1.length; i++) {
-        var date = data1[i].current.dt
-        console.log(date)
+    for (let i = 1 ; i < 6; i++) {
+        var dateDiv = document.getElementById(`date${i}`)
+        var date = data1.daily[i].dt
         var reformatDate = moment(date, "X" ).format("l")
-        dateEl.textContent = reformatDate
-        // dateEl.append(reformatDate)
-        
+        dateDiv.textContent = reformatDate
+        var tempDiv = document.getElementById(`temp${i}`)
+        var temps = data1.daily[i].temp.max
+        tempDiv.textContent = "Temp: " + temps + " °F"
+        var windDiv = document.getElementById(`wind${i}`)
+        var windy = data1.daily[i].wind_speed
+        windDiv.textContent = "Wind: " + windy +" MPH"
+        var humDiv = document.getElementById(`hum${i}`)
+        var humid = data1.daily[i].humidity
+        humDiv.textContent = "Humidity " + humid +" %"
+
     }
 }).catch(function (error) {
 	console.warn(error);
 });
+}
+
+BtnSubmit.addEventListener("click", function(event){
+    event.preventDefault();
+    console.log(citySearch.value)
+    apiCity(citySearch.value)
+    if (searchCities.includes(citySearch.value)) {
+        return
+    }else {
+        searchCities.push(citySearch.value)
+        var cityHistory = document.getElementById("searchHistory")
+        var btn = document.createElement("button")
+        btn.innerText = citySearch.value
+        btn.value = citySearch.value
+        cityHistory.appendChild(btn)
+        localStorage.setItem("cities", JSON.stringify(searchCities))
+    }
+
+})
+
+for (let j = 0; j < searchCities.length; j++) {
+var cityHistory = document.getElementById("searchHistory")
+var btn = document.createElement("button")
+btn.innerText = searchCities[j]
+btn.value = searchCities[j]
+cityHistory.appendChild(btn)
+    
+}
